e148553b3217fe51087eec3baff3043d3bfce3bc
Athena Hosseini <athena.hosseini01@gmail.com>
2025-11-18 14:57:00 +0100

    Final fixes for assignment

--- GuessingGame.java
+++ GuessingGame.java
@@ -1,7 +1,7 @@
 public class GuessingGame {
     public static void main(String[] args) {
         int low = 0;
-        int high = 10; // snabbtest, som i lärarens exempel
+        int high = 10;
 
         Guesser g = new Guesser(low, high);
         g.start();
