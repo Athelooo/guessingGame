c9434500a6ceebe8576220452d6c0b1619119d39
Athena Hosseini <athena.hosseini01@gmail.com>
2025-11-18 13:55:05 +0100

    Fixat GuessingGame enligt instruktionerna

--- GuessingGame.java
+++ GuessingGame.java
@@ -1,9 +1,9 @@
 public class GuessingGame {
     public static void main(String[] args) {
         int low = 0;
-        int high = 1000;  // vi kan ändra till 10 senare för snabbtest
+        int high = 10; // snabbtest, som i lärarens exempel
+
         Guesser g = new Guesser(low, high);
-        // g.start(); // lägger vi till i nästa steg
-        System.out.println("GuessingGame compiled OK. Constructor works.");
+        g.start();
     }
 }
